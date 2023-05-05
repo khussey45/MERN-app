e945974eee5e1ddba7172668c3aa685b006b7dab
khussey45 <khussey45@gmail.com>
2023-05-05 13:23:44 -0400

    d

--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 // Configurations (middleware)
 const __filename = fileURLToPath(import.meta.url);
@@ -40,6 +42,7 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
+app.post(".posts", verifyToken, upload.single("picture"), createPost);
 
 // ROUTES
 app.use("/auth", authRoutes);
