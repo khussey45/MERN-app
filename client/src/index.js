c34801fa4d46e1cda11949c215c6b780b9e3e3ce
Kieren James Hussey <khussey45@gmail.com>
2023-05-12 10:04:52 -0400

    fe

--- client/src/index.js
+++ client/src/index.js
@@ -32,9 +32,12 @@ const store = configureStore({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
     </Provider>
+      
   </React.StrictMode>
 );
 
